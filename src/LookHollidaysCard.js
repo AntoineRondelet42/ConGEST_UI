9fcea262e6e33dac13539ab39e570c249e3d4eed
AntoineRondelet42 <antoine.rondelet@outlook.fr>
2022-05-23 01:04:50 +0200

    suppr lignes import useless

--- src/LookHollidaysCard.js
+++ src/LookHollidaysCard.js
@@ -1,14 +1,7 @@
-import { ButtonGroup } from '@mui/material';
 import React from 'react';
-import axios from 'axios';
-import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import TextField from "@mui/material/TextField";
 import "./LookHollidays.scss"
 import Moment from 'react-moment';
 
